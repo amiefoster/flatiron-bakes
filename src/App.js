15872270d1f80778f6645eb6dbc911f68122510e
Amie Foster <amie.n.foster@gmail.com>
2021-09-21 16:09:23 -0600

    added new componant for data cakes array

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
 import CakeCard from "./Cakecard.js";
 import Header from "./Header";
 import Search from "./Search";
-
-const cakes = [
-  {
-      flavor: 'Vanilla',
-      size: '6" cake',
-      price: 40.00
-  },
-  {
-      flavor: 'Raspberry Cardamon Rose',
-      size: '9" cake',
-      price: 50.00
-  },
-  {
-      flavor: 'Pink Champagne',
-      size: 'cup cake',
-      price: 37.5
-  },
-  {
-      flavor: 'Earl Grey',
-      size: 'cup cake',
-      price: 18.00
-  },
-{
-      flavor: 'Black Forest',
-      price: 18.00
-  }
-]
+import cakes from "./Data";
 
 function App() {
   return (
---
+++ src/Data.js
@@ -0,0 +1,28 @@
+const cakes = [
+    {
+        flavor: 'Vanilla',
+        size: '6" cake',
+        price: 40.00
+    },
+    {
+        flavor: 'Raspberry Cardamon Rose',
+        size: '9" cake',
+        price: 50.00
+    },
+    {
+        flavor: 'Pink Champagne',
+        size: 'cup cake',
+        price: 37.5
+    },
+    {
+        flavor: 'Earl Grey',
+        size: 'cup cake',
+        price: 18.00
+    },
+  {
+        flavor: 'Black Forest',
+        price: 18.00
+    }
+  ]
+
+  export default cakes;
